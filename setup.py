8fde30875a3e5e16c91284e7493bf1a3fdce3a73
Jonatan Olofsson <jonatan.olofsson@gmail.com>
2018-03-16 09:18:46 +0100

    Add systemd service

--- setup.py
+++ setup.py
@@ -1,10 +1,24 @@
 """Setup module for hearth"""
-
+import os
+import shutil
 from setuptools import find_packages, setup
+from setuptools.command.install import install
+
+THIS_DIR = os.path.dirname(os.path.abspath(__file__))
+
+
+class CustomInstallCommand(install):
+    """Custom install command."""
+
+    def run(self):
+        """Run."""
+        install.run(self)
+        shutil.copyfile(THIS_DIR + '/hearth.service', '/lib/systemd/system')
+
 
 setup(
     name="hearth",
-    version="0.1.0",
+    version="0.2.0",
     description="Home Controller",
     url="http://github.com/jonatanolofsson/hearth",
     author="Jonatan Olofsson",
@@ -14,14 +28,8 @@ setup(
     entry_points={
         'console_scripts': ['hearth=hearth:main'],
     },
-    install_requires=[
-        'hbmqtt',
-        'asyncinit',
-        'git+https://github.com/jonatanolofsson/schedule.git',
-        'aiohttp',
-        'astral',
-    ],
     tests_require=[
         'pytest',
     ],
+    cmdclass={'install': CustomInstallCommand}
 )
