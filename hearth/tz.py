f297e82b1e499a26b5dfe4bf2422dce7cfb711f0
Jonatan Olofsson <jonatan.olofsson@gmail.com>
2018-04-02 18:26:19 +0200

    Add daylight/is_dark to tz module

--- hearth/tz.py
+++ hearth/tz.py
@@ -1,5 +1,8 @@
+"""Hearth timezone module."""
 import sys
+from datetime import timedelta, datetime
 import astral
+import pytz
 
 
 class TZ:
@@ -10,6 +13,28 @@ class TZ:
     def set_locale(self, name, region, lat, lon, tzname, elevation):
         """Set locale."""
         self.location = astral.Location((name, region, lat, lon, tzname, elevation))
+
+    def daylight(self, when=None, delta=None):
+        """Return true if time is within daylight hours
+
+        (plus a timedelta in the morning, minus the same timedelta in the evening)."""
+        when = when or self.now()
+        delta = delta or timedelta(minutes=30)
+        return self.sunrise(when) + delta < when < self.sunset(when) - delta
+
+    def is_dark(self, when=None, delta=None):
+        """Return true if time is within dark hours
+
+        (plus a timedelta in the evening, minus the same timedelta in the morning)."""
+        return not self.daylight(when, delta)
+
+    def now(self):
+        """Return localized time."""
+        return self.localize(datetime.now())
+
+    def localize(self, time):
+        """Localize to local time."""
+        return pytz.timezone(self.location.timezone).localize(time)
 
     def __getattr__(self, attr):
         """Getattr"""
